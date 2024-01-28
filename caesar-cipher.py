53aa9d5c02a9920238680ec8182fd95e9eeb97ca
Kamran Azim <Kamazim121212@gmail.com>
2024-01-28 23:07:39 +0000

    Created Working Version

--- caesar-cipher.py
+++ caesar-cipher.py
@@ -1,1 +1,22 @@
-alphabet = ""
+letters = list("abcdefghijklmnopqrstuvwxyz")
+message = input("Enter a message: ").lower()
+shift = False
+
+
+while not shift:
+    try:
+        shift = int(input("Enter a shift number: "))
+    except ValueError:
+        print("Enter a Number!")
+
+
+new_message = ""
+for char in message:
+    if char in letters:
+        index = letters.index(char) - shift
+        new_message += letters[index]
+    else:
+        new_message += char 
+
+
+print(new_message)
